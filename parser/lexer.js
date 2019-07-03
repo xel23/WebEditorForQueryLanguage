f715baa1a363fab62c93c970d0a974ffbe07df09
igor.kuzminov <igor.kuzminov@jetbrains.com>
2019-07-03 11:51:56 +0300

    class lexer

--- parser/lexer.js
+++ parser/lexer.js
@@ -1,82 +1,78 @@
-
-
-// class Lexer {
-//     constructor(str) {
-//         this.str = str;
-//     }
-//
-//
-// }
-
-function lexer(str) {
-    let tokens = [];
-
-    let isOperator = function (c) {
-        return /[:;\^\$\.\|\?\*\+\(\)/]/.test(c);
-    };
-
-    let isWhiteSpace = function (c) {
-        return /[\s]/.test(c);
-    };
-
-    let isSymbol = function (c) {
-        return typeof c === "undefined" ? false : /[a-zA-Z_]/.test(c);
-    };
-
-    let isDigit = function (c) {
-        return /[\d]/.test(c);
-    };
-
-    let addToken = function (type, value) {
-        tokens.push({
-            type: type,
-            value: value
-        })
-    };
-
-    let c, i = 0;
-
-    let advance = function () {
-        return c = str[++i];
-    };
-
-    while (i < str.length) {
-        c = str[i];
-        if (isWhiteSpace(c)) {
-            advance();
-        }
-
-        else if (isSymbol(c)) {
-            let word = c;
-            while (isSymbol(advance())) {
-                word += c;
-
-            }
-
-            addToken('word', word);
-        }
-
-        else if (isDigit(c)) {
-            let num = c;
-            while (isDigit(advance())) {
-                num += c;
-            }
-
-            num = parseFloat(num);
-
-            addToken('number', num);
-        }
-        else if (isOperator(c)) {
-            addToken('operator', c);
-            advance();
-        }
-        else {
-            throw "Unrecognized token";
-        }
+class Lexer {
+    constructor(str) {
+        this.str = str;
     }
 
-    addToken('(end)');
-    return tokens;
+    lexer() {
+        let tokens = [];
+
+        let isOperator = function (c) {
+            return /[:;\^\$\.\|\?\*\+\(\)/]/.test(c);
+        };
+
+        let isWhiteSpace = function (c) {
+            return /[\s]/.test(c);
+        };
+
+        let isSymbol = function (c) {
+            return typeof c === "undefined" ? false : /[a-zA-Z_]/.test(c);
+        };
+
+        let isDigit = function (c) {
+            return /[\d]/.test(c);
+        };
+
+        let addToken = function (type, value) {
+            tokens.push({
+                type: type,
+                value: value
+            })
+        };
+
+        let c, i = 0;
+
+        let advance = function () {
+            return c = this.str[++i];
+        };
+
+        while (i < this.str.length) {
+            c = this.str[i];
+            if (isWhiteSpace(c)) {
+                advance.bind(this)();
+            }
+
+            else if (isSymbol(c)) {
+                let word = c;
+                while (isSymbol(advance.bind(this)())) {
+                    word += c;
+
+                }
+
+                addToken('word', word);
+            }
+
+            else if (isDigit(c)) {
+                let num = c;
+                while (isDigit(advance.bind(this)())) {
+                    num += c;
+                }
+
+                num = parseFloat(num);
+
+                addToken('number', num);
+            }
+            else if (isOperator(c)) {
+                addToken('operator', c);
+                advance.bind(this)();
+            }
+            else {
+                throw "Unrecognized token";
+            }
+        }
+
+        addToken('(end)');
+        return tokens;
+    }
 }
 
-module.exports = lexer;
+module.exports = Lexer;
