97dbd2b2cfa688f6500100cba00f5f423f809010
igor.kuzminov <igor.kuzminov@jetbrains.com>
2019-07-08 10:29:01 +0300

    update lexer for query grammar

--- parser/lexer.js
+++ parser/lexer.js
@@ -1,116 +1,6 @@
-// TO DO:
-// 1. add {} for fieldValue
-// 2. continue working on for query grammar
-// 3. handler for whitespace
-
 let operators = require('./operators');
 let symbols = require('./symbols');
 let types = require('./types');
-
-// class Lexer {
-//     constructor(str) {
-//         this.str = str;
-//         this.start = 0;
-//         this.current = 0;
-//         this.tokens = [];
-//     }
-//
-//     scanTokens() {
-//         while (!this.isAtEnd()) {
-//             this.start = this.current;
-//             this.scanToken();
-//         }
-//
-//         this.tokens.push(new Token(operators.EOF, "", null));
-//         return this.tokens;
-//     }
-//
-//     scanToken() {
-//         let c = this.advance();
-//             switch (c) {
-//                 case '(': this.addToken(operators.LEFT_PAREN); break;
-//                 case ')': this.addToken(operators.RIGHT_PAREN); break;
-//                 case '\.': this.addToken(operators.DOT); break;
-//                 case '-': this.addToken(operators.MINUS); break;
-//                 case '\+': this.addToken(operators.PLUS); break;
-//                 case ';': this.addToken(operators.SEMICOLON); break;
-//                 case '\*': this.addToken(operators.STAR); break;
-//                 case '\/': this.addToken(operators.SLASH); break;
-//                 case '!': this.addToken(this.match('=') ? operators.BANG_EQUAL : operators.BANG); break;
-//                 case '=': this.addToken(this.match('=') ? operators.EQUAL_EQUAL : operators.EQUAL); break;
-//                 case '<': this.addToken(this.match('=') ? operators.LESS_EQUAL : operators.LESS); break;
-//                 case '>': this.addToken(this.match('=') ? operators.GREATER_EQUAL :
-//                     operators.GREATER); break;
-//                 case ':': this.addToken(operators.COLON); break;
-//                 case ' ':
-//                 case '\r':
-//                 case '\t':
-//                 case '\n': break;
-//                 default: if (this.isDigit(c)) {
-//                     this.number();
-//                 } else {
-//                     throw 'Unexpected token';
-//                 }
-//             }
-//     }
-//
-//     isAtEnd() {
-//         return this.current >= this.str.length;
-//     }
-//
-//     advance() {
-//         this.current++;
-//         return this.str[this.current - 1];
-//     }
-//
-//     addToken(type) {
-//         if (arguments[1] === undefined) {
-//             this.tokens.push(new Token(type, null));
-//         }
-//
-//         else if (typeof arguments[1] === 'object'){
-//             let text = this.str.substring(this.start - this.current);
-//             this.tokens.push(new Token(type, text, arguments[1]));
-//         }
-//
-//         else {
-//             this.tokens.push(new Token(type, arguments[1].toString(), arguments[1]));
-//         }
-//
-//     }
-//
-//     match(expected) {
-//         if (this.isAtEnd()) return false;
-//         if (this.str[this.current] !== expected) return false;
-//
-//         this.current++;
-//         return true;
-//     }
-//
-//     isDigit(c) {
-//         return /[\d]/.test(c);
-//     };
-//
-//     number() {
-//         while (this.isDigit(this.peek())) this.advance();
-//         if (this.peek() === '.' && this.isDigit(this.peekNext())) {
-//             this.advance();
-//
-//             while (this.isDigit(this.peek())) this.advance();
-//         }
-//         this.addToken('NUMBER', parseFloat(this.str.substring(this.start, this.current)));
-//     }
-//
-//     peek() {
-//         if (this.isAtEnd()) return '\0';
-//         return this.str[this.current];
-//     }
-//
-//     peekNext() {
-//         if (this.current + 1 >= this.str.length) return '\0';
-//         return this.str[this.current + 1];
-//     }
-// }
 
 class Token {
     constructor(type, lexeme, literal) {
@@ -124,24 +14,7 @@ class Token {
     }
 }
 
-const keywords = Object.freeze({
-    AND: 'and', // 1
-    IF: 'if', // 2
-    OR: 'or', // 3
-    FALSE: 'false', // 4
-    TRUE: 'true', // 5
-    PROJECT: 'project' // 6
-});
-
-
-
-
-
-
-
-
 // Below is new lexer for Hub query grammar
-
 class Lexer {
     constructor(str) {
         this.str = str;
@@ -168,15 +41,19 @@ class Lexer {
         switch (c) {
             case '(': this.addToken(operators.LEFT_PAREN); break;
             case ')': this.addToken(operators.RIGHT_PAREN); break;
-            case '{': this.string(); break;
+            case '{': this.addToken(types.STRING, this.string()); break;
             case ',': this.addToken(symbols.COMMA); break;
+            case ' ':
+            case '\t':
+            case '\n':
+            case '\r': break;
             default: {
                 if (this.isDigit(c)) {
                     this.number();
                 } else if (this.isAlpha(c)) {
                     this.identifier();
                 } else {
-                    throw "Unexpected token";
+                    throw "Unexpected token ";
                 }
             }
         }
@@ -256,16 +133,21 @@ class Lexer {
             throw "Unterminated string";
         }
 
-        this.addToken(types.STRING, this.str.substring(this.start + 1, this.current - 1));
-
         this.advance();
+        return this.str.substring(this.start + 1, this.current - 1);
     }
 
     isAlpha(c) {
-        return /[a-zA-Z_]/.test(c);
+        return /[a-zA-Z_\-]/.test(c);
     }
 
     identifier() {
+        while (this.peek() === ' ' || this.peek() === '\t' || this.peek() === '\n' || this.peek() === '\r') {
+            this.advance();
+            this.start = this.current;
+        }
+
+
         while (this.isAlphaNumeric(this.peek())) this.advance();
 
         if (this.str[this.current] === ':') {
@@ -279,13 +161,36 @@ class Lexer {
 
             this.addToken(types.FIELD_VALUE, fieldValue);
         }
-
+        else if (this.str[this.current] === '(') {
+            this.addToken(types.TUPLE_NAME, this.str.substring(this.start, this.current));
+            this.advance();
+            this.start = this.current;
+            this.addToken(operators.LEFT_PAREN);
+            this.scanToken();
+        }
+        else if (this.str.substring(this.start, this.current).toUpperCase() in operators) {
+            this.addToken(types.KEYWORD, this.str.substring(this.start, this.current));
+        }
+        else {
+            this.addToken(types.FIELD_VALUE, this.str.substring(this.start, this.current));
+        }
     }
 
-    fieldValueIdentifier(start) {
+    fieldValueIdentifier() {
+        while (this.peek() === ' ' || this.peek() === '\t' || this.peek() === '\n' || this.peek() === '\r') {
+            this.advance();
+            this.start = this.current;
+        }
+
+        let cur;
+        if (this.peek() === '{') {
+            cur = this.string();
+            return cur;
+        }
+
         while (this.isAlphaNumeric(this.peek())) this.advance();
 
-        return this.str.substring(start, this.current);
+        return this.str.substring(this.start, this.current);
     }
 
     isAlphaNumeric(c) {
@@ -294,3 +199,10 @@ class Lexer {
 }
 
 module.exports = Lexer;
+
+try {
+    let t = new Lexer('login: {darth.vader}, yoda access(project: DS,with: Developer)');
+    console.log(t.scanTokens());
+} catch (e) {
+    console.log(e);
+}
